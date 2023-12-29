1ca8a26dea28bd77dffeb13b58762e5114d1b207
Racianu Marian Gabriel <94560373+RacianuGabriel@users.noreply.github.com>
2023-12-29 10:52:21 +0200

    Added navigation to table

--- bankingApp/bankingApp/pages/adminPages/UserListPage.xaml.cs
+++ bankingApp/bankingApp/pages/adminPages/UserListPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,17 +21,57 @@ namespace bankingApp.pages.adminPages
     /// <summary>
     /// Interaction logic for UserListPage.xaml
     /// </summary>
-    public partial class UserListPage : Page
+
+    public class UserListDataContext : INotifyPropertyChanged
+    {
+        private int _currentPage;
+        private int _numberOfPages;
+
+        public int CurrentPage
+        {
+            get { return _currentPage; }
+            set
+            {
+                if (_currentPage != value)
+                {
+                    _currentPage = value;
+                    OnPropertyChanged("CurrentPage");
+                }
+            }
+        }
+
+        public int NumberOfPages
+        {
+            get { return _numberOfPages; }
+            set
+            {
+                if (_numberOfPages != value)
+                {
+                    _numberOfPages = value;
+                    OnPropertyChanged("NumberOfPages");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public partial class UserListPage : Page, INotifyPropertyChanged
     {
         public bool isDarkTheme { get; set; }
         private readonly PaletteHelper _paletteHelper = new PaletteHelper();
         bsappDataContext db;
-        
         public UserListPage(bool isDarkTheme, PaletteHelper _paletteHelper, bsappDataContext db)
         {
             this.isDarkTheme = isDarkTheme;
             this._paletteHelper = _paletteHelper;
             this.db = db;
+            this.DataContext = new UserListDataContext();
             InitializeComponent();
             InitializeDataGrid();
         }
@@ -40,7 +82,10 @@ namespace bankingApp.pages.adminPages
                 .ToList();
             lblUsers.Text = $"{users.Count} Users";
             userTable.ItemsSource = users;
+            ((UserListDataContext)this.DataContext).NumberOfPages = users.Count/10;
+            if (users.Count % 10 != 0) ((UserListDataContext)this.DataContext).NumberOfPages++; 
         }
+        
         private void toggleTheme(object sender, RoutedEventArgs e)
         {
             ITheme theme = _paletteHelper.GetTheme();
@@ -57,5 +102,24 @@ namespace bankingApp.pages.adminPages
             }
             _paletteHelper.SetTheme(theme);
         }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises this object's PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">The property that has a new value.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+        #endregion
     }
 }
