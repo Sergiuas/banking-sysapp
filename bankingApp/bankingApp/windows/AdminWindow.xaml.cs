8de4a92483cfdec607ce3f6ce8c7e3659eb7c0e5
Racianu Marian Gabriel <94560373+RacianuGabriel@users.noreply.github.com>
2024-01-11 13:44:59 +0200

    Added AddManagerPage

--- bankingApp/bankingApp/windows/AdminWindow.xaml.cs
+++ bankingApp/bankingApp/windows/AdminWindow.xaml.cs
@@ -129,5 +129,17 @@ namespace bankingApp.windows
             SettingsPage Page = new SettingsPage(isDarkTheme, _paletteHelper, db);
             page.Content = Page;
         }
+
+        private void btnAddManager_Click(object sender, RoutedEventArgs e)
+        {
+            editManagerPage Page = new editManagerPage(isDarkTheme, _paletteHelper, db);
+            page.Content = Page;
+        }
+
+        private void btnEditManager_Click(object sender, RoutedEventArgs e)
+        {
+            editManagerPage Page = new editManagerPage(isDarkTheme, _paletteHelper, db);
+            page.Content = Page;
+        }
     }
 }
