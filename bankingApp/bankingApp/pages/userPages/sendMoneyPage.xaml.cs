27cfe3799a7f88005181904f4dd403b4d5660c5d
Racianu Marian Gabriel <94560373+RacianuGabriel@users.noreply.github.com>
2024-01-12 11:02:52 +0200

    Minor Modification

---
+++ bankingApp/bankingApp/pages/userPages/sendMoneyPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bankingApp.pages.userPages
+{
+    /// <summary>
+    /// Interaction logic for sendMoneyPage.xaml
+    /// </summary>
+    public partial class sendMoneyPage : Page
+    {
+        bsappDataContext db;
+        public sendMoneyPage(bsappDataContext db)
+        {
+            this.db = db;
+            InitializeComponent();
+        }
+
+        private void btnFirstPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPrevPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNextPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLastPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ bankingApp/bankingApp/pages/userPages/transactionsPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bankingApp.pages.userPages
+{
+    /// <summary>
+    /// Interaction logic for transactionsPage.xaml
+    /// </summary>
+    public partial class transactionsPage : Page
+    {
+
+        bsappDataContext db;
+        public transactionsPage(bsappDataContext db)
+        {
+            this.db = db;
+            InitializeComponent();
+        }
+
+        private void btnFirstPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPrevPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNextPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLastPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
