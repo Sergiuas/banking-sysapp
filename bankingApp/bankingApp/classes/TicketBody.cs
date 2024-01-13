7f2d24c1a474545077261dc6fdb58766826917dd
Racianu Marian Gabriel <94560373+RacianuGabriel@users.noreply.github.com>
2024-01-13 03:53:31 +0200

    Added Ticket system

---
+++ bankingApp/bankingApp/classes/TicketBody.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankingApp.classes
+{
+    internal class TicketBody
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string subject { get; set; }
+        public string body { get; set; }
+        public DateTime date { get; set; }
+        public bool status { get; set; }
+        public TicketBody()
+        {
+            id = 0;
+            username = "username";
+            subject = "subject";
+            body = "bodyasdassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss";
+            date = DateTime.Now;
+            status = false;
+        }
+    }
+}
