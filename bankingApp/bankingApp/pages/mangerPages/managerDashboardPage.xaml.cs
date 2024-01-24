b5b2308f4dd0682fe63cd76c4b7debc7bd1f3ec6
Sergiu Amzuloiu <sergiuamz@gmail.com>
2024-01-25 00:45:47 +0200

    push

--- bankingApp/bankingApp/pages/mangerPages/managerDashboardPage.xaml.cs
+++ bankingApp/bankingApp/pages/mangerPages/managerDashboardPage.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using bankingApp.classes;
 
 namespace bankingApp.pages.mangerPages
 {
@@ -23,6 +25,10 @@ namespace bankingApp.pages.mangerPages
     public partial class managerDashboardPage : Page
     {
         bsappEntities db;
+        public int solvedCount;
+        public int unsolvedCount;
+        public int activeCount;
+        public int pendingCount;
         public managerDashboardPage(bsappEntities db)
         {
             this.db = db;
@@ -30,7 +36,31 @@ namespace bankingApp.pages.mangerPages
             PointLabel = chartPoint =>
                 string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
 
+
+           this.solvedCount = (from t in db.Tickets
+                               where t.ManagerID == UserSingleton.Instance.UserID && t.Resolved==true
+                               select t).Count();
+
+            this.unsolvedCount = (from t in db.Tickets
+                                 where t.ManagerID == UserSingleton.Instance.UserID && t.Resolved == false
+                                 select t).Count();
+
+            this.pendingCount = (from c in db.Cards
+                                where c.ManagerID==UserSingleton.Instance.UserID && c.Active==false
+                                select c).Count();
+
+            this.activeCount = (from c in db.Cards
+                               where c.ManagerID == UserSingleton.Instance.UserID && c.Active == true
+                               select c).Count();
+
+            
             DataContext = this;
+
+            pieActiveCards.Values = new LiveCharts.ChartValues<int> { this.activeCount };
+            piePendingCards.Values = new LiveCharts.ChartValues<int> { this.pendingCount };
+            pieTicketsSolved.Values = new LiveCharts.ChartValues<int> { this.solvedCount };
+            pieTicketsUnsolved.Values = new LiveCharts.ChartValues<int> { this.unsolvedCount };
+
         }
 
         public Func<ChartPoint, string> PointLabel { get; set; }
